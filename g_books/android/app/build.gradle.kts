ba3edf102945bb1353b22f7022aa57f61382c72f
SunnyShoun <69196119+sunnyshoun@users.noreply.github.com>
2026-06-07 05:32:43 +0800

    update app

--- g_books/android/app/build.gradle.kts
+++ g_books/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.g_books"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
