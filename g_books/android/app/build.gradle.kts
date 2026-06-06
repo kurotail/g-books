19a1db8d513c59bb26d9da533017717a12d4ffa4
SunnyShoun <69196119+sunnyshoun@users.noreply.github.com>
2026-06-07 00:58:24 +0800

    update assets struct

--- g_books/android/app/build.gradle.kts
+++ g_books/android/app/build.gradle.kts
@@ -19,7 +19,8 @@ android {
         applicationId = "com.example.g_books"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        // App targets Android 14 (API 34) and above.
+        minSdk = 34
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
